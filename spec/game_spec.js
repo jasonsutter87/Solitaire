61cabbfcb02e1f84eb13b4bea3a562d6cfca5fb3
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2016-10-24 08:35:50 -0700

    Added Spec Folder

---
+++ spec/game_spec.js
@@ -0,0 +1,20 @@
+describe("generates a deck of cards", function() {
+  // beforeEach(function() {
+  //   game = new Game();
+  //   gameBoard = [[],[],[],[],[],[],[]]
+    
+  // });
+
+  // it("board should not be null", function() {
+  //    expect(game.board).not.toBeNull();
+  // });
+
+  // it("board should should have 42 spots", function() {
+  //    expect(game.board.length).toEqual(7);
+  // });
+
+  // it("board should be fill will all empty strings", function() {
+  //    expect(game.board).toEqual(gameBoard);
+  // });
+});
+
