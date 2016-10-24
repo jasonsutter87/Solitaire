b9e6fadd792e6d7d033acc4a47a44c43d4855535
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2016-10-24 08:35:40 -0700

    Added phesdocode for game js

---
+++ js/game.js
@@ -0,0 +1,32 @@
+// New Game
+//  => Generates a standard deck of cards ( 1-13, (C, S, H, D))
+
+//reset game
+//  => new game, shuffle deck, then deal
+
+//Shuffle Deck
+//  => randomizes the 65 cards
+
+//Set up game (Initial deal)
+//  => places cards 7 sets (1-7)
+//  => rest card place in turn deal group.
+
+//Deal hand (turn deal)
+//  => Deal 3
+//  => Deal 1
+
+
+//Last played card
+//  => Was the last card played (in the spot, empty, Red or Black?)
+
+//Can be place?
+//  => Yes, No
+
+
+//game over
+
+//place card in pile
+
+//move groups of card from one pile to another [ array1, => array3, or array1-card2-4, => array3 ]
+
+
