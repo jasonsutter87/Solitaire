5e1201e5f719e79b2c9b5a09b350716e16da8fe5
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2016-11-06 14:43:58 -0800

    Wrote canPlace function

--- js/game.js
+++ js/game.js
@@ -76,15 +76,113 @@ var Game = function(cards){
 	this.deal[0] = this.deck.deck
 }
 
+Game.prototype.moveCards = function(fromX, fromY, newSpotIndex) {
+	var cardX = fromX 
+	var cardY = fromY 
+	var cardIndex = newSpotIndex
+	var tempGroup = []
 
 
+	//Moves single card over
+	// console.log("Moving cards")
+	// this.spots[cardIndex].push(this.spots[cardX][cardY])
+	// this.spots[cardX].shift()
+
+
+	//Moves single card over
+	console.log("Moving cards 2.0")
+	// console.log(this.spots[cardX][cardY])
+	// console.log(this.spots[newSpotIndex])
+
+
+
+
+
+
+
+
+
+
+	//checks the cards from the one picked until the end.
+	// moved the one picked until the end into a temportory array
+	//delete the one picked item column in the array until the end.
+	//moved the first card  item column in the array until the end into the coulm picked.
+
+
+
+
+	// this.spots[cardIndex].push(this.spots[cardX][cardY])
+	// this.spots[cardX].shift()
+
+	
+	
+	console.log(this.spots)
+}
+
+
+
+Game.prototype.canPlaced = function(cardA, cardB){
+// TODO: Kings can only be placed on empty arrays
+	var topCard = cardA
+	var placingCard = cardB
+
+	var suitA = topCard.split('', 1)[0]
+	var suitB = placingCard.split('', 1)[0]
+
+	if(topCard.length == 2){
+		var valueA = topCard.slice(-1)
+	}else{
+		var valueA = topCard.slice(-2)
+	}
+
+	if(placingCard.length == 2){
+		var valueB = placingCard.slice(-1)
+	}else{
+		var valueB = placingCard.slice(-2)
+	}
+
+	if(suitA != suitB){
+		if(suitA == "H" && suitB != "D"){
+			if(valueA - 1 == valueB){
+				console.log("VALID MOVE")
+				return true
+			}
+		}
+		if(suitA == "D" && suitB != "H"){
+			if(valueA - 1 == valueB){
+				console.log("VALID MOVE")
+				return true
+			}
+		}
+		if(suitA == "S" && suitB != "C"){
+			if(valueA - 1 == valueB){
+				console.log("VALID MOVE")
+				return true
+			}
+		}
+		if(suitA == "C" && suitB != "S"){
+			if(valueA - 1 == valueB){
+				console.log("VALID MOVE")
+				return true
+			}
+		}else{
+			console.log("NOT A VALID MOVE")
+			return false
+		}
+	} else{
+		console.log("NOT A VALID MOVE")
+		return false
+	}
+}
 
 
 
 // ++++++++++++ DRIVER CODE ++++++++++++
 deck = new Deck()
 game = new Game(deck)
-// console.log(game)
+
+
+game.canPlaced("H13", "D2")
 
 
 
@@ -105,8 +203,7 @@ game = new Game(deck)
 //Last played card
 //  => Was the last card played (in the spot, empty, Red or Black?)
 
-//Can be place?
-//  => Yes, No
+
 
 
 //game over
